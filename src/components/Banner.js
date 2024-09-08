074783ea92e73eae3ce5a977ada6c38a55622359
Muhammad Siddik Akabar <siddik20@navgurukul.org>
2024-09-08 09:38:32 +0530

    add link to banners

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "../assets/styles/Banner.css"; // Import the CSS styles
+import { Link } from "react-router-dom";
 
 const Banner = ({ bannerImg }) => {
 	return (
-		<div className="banner">
-			{" "}
-			{/* Container for the banner */}
-			<img src={bannerImg} alt="Promotional Banner Image" />{" "}
-			{/* Banner image */}
-		</div>
+		<Link to={`/shop`} className="edit-link">
+			<div className="banner">
+				{" "}
+				{/* Container for the banner */}
+				<img src={bannerImg} alt="Promotional Banner Image" />{" "}
+				{/* Banner image */}
+			</div>
+		</Link>
 	);
 };
 
